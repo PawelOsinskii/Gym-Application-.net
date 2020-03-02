7c4322eef5c66256562f34b85fc858c7218a7f77
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-03 00:59:57 +0100

    Add project files.

---
+++ Projekcik.NETS/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,34 @@
+﻿
+using Projekcik.NETS.Models.Data;
+using Projekcik.NETS.Views.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Projekcik.NETS.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // deklaracja listy pagewievmodel
+            List<PageVM> pagesList;
+
+            
+            using (Db db = new Db())
+            {// inicjalizacja danymi z bazy danych listy
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+
+
+            }
+                //zzwracamy strony do widoku
+            return View(pagesList);
+        }
+        //Get: Admin/pages/addpages
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projekcik.NETS/Views/Pages/PageVM.cs
@@ -0,0 +1,37 @@
+﻿using Projekcik.NETS.Models.Data;
+using System.ComponentModel.DataAnnotations;
+namespace Projekcik.NETS.Views.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+
+        }
+
+        public PageVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =3)]
+        [Display(Name = "Tytuł strony")]
+        public string Title { get; set; }
+        [Display(Name = "adres strony")]
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        [Display(Name = "zawartość strony")]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        [Display(Name = "pasek boczny")]
+        public bool HasSidebar { get; set; }
+    }
+}
