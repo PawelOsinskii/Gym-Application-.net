9aeabd173fc8d961460f767ec23e9f698b6156dd
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-03 01:30:15 +0100

    admin pages

--- Projekcik.NETS/Areas/Admin/Controllers/PagesController.cs
+++ Projekcik.NETS/Areas/Admin/Controllers/PagesController.cs
@@ -26,9 +26,56 @@ namespace Projekcik.NETS.Areas.Admin.Controllers
             return View(pagesList);
         }
         //Get: Admin/pages/addpages
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //sprawdzanie walidacji formularza
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (Db db = new Db())
+            {
+                string slug;
+                //inicjalizacja pagedto
+                PageDTO dto = new PageDTO();
+                
+                
+                //gdy slug jest pusty, to przypisujemy tytuł
+                if(string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+                //zapobiegamy dodania takiej samej nazwy strony
+                if ((db.Pages.Any(x => x.Title == model.Title)) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "Ten tytuł już istnieje");
+                    return View(model);
+                }
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 1000;
+
+
+                //zapis dto
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Dodałeś nową stronę";
+            return RedirectToAction("AddPage");
+        }
+
     }
 }
