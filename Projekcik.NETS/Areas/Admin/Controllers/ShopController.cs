c5548a69fc506f2e850d9903a0504720ccdacd9d
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-12 22:58:57 +0100

    Kategorie

--- Projekcik.NETS/Areas/Admin/Controllers/ShopController.cs
+++ Projekcik.NETS/Areas/Admin/Controllers/ShopController.cs
@@ -1,17 +1,28 @@
-﻿using System;
+﻿using Projekcik.NETS.Models.Data;
+using Projekcik.NETS.Models.ViewModels.Shop;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Projekcik.NETS.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
         {
-            return View();
+            //deklaracja listy kategorii do wyswietlenia
+            List<CategoryVM> categoryVMList;
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories
+                                    .ToArray()
+                                    .OrderBy(x => x.Sorting)
+                                    .Select(x => new CategoryVM(x)).ToList();
+
+            }
+
+            return View(categoryVMList);
         }
     }
 }
