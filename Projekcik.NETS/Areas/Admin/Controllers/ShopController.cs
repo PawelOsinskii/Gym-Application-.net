68f422473eabfa74be2a2f46730317696d9af9c5
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-20 21:28:15 +0100

    usuwanie zdjęc z galerii, koniec modułu admin

--- Projekcik.NETS/Areas/Admin/Controllers/ShopController.cs
+++ Projekcik.NETS/Areas/Admin/Controllers/ShopController.cs
@@ -506,5 +506,20 @@ namespace Projekcik.NETS.Areas.Admin.Controllers
             }
             return View();
         }
+
+        // Post: Admin/Shop/deleteImage
+        [HttpPost]
+        public void deleteImage(int id, string imageName)
+        {
+            string fullPath1 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/" + imageName);
+            string fullPath2 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/Thumbs/" + imageName);
+
+            if (System.IO.File.Exists(fullPath1))
+                System.IO.File.Delete(fullPath1);
+
+            if (System.IO.File.Exists(fullPath2))
+                System.IO.File.Delete(fullPath2);
+        }
+
     }
 }
