103e5eb5b3f2a319acf2b6c12c4a78dc41aa2c34
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-21 15:05:36 +0100

    Dodanie mechanizmu wyświetlania stron FRONTPAGES

--- Projekcik.NETS/App_Start/RouteConfig.cs
+++ Projekcik.NETS/App_Start/RouteConfig.cs
@@ -12,12 +12,14 @@ namespace Projekcik.NETS
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "Projekcik.NETS.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "Projekcik.NETS.Controllers"});
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+         //   routes.MapRoute(
+             //   name: "Default",
+        //        url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        //    );
         }
     }
 }
