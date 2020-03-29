d11f05a67728936b4908c7a1ef65574940fc0e2e
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-29 15:51:27 +0200

    Wyświetlanie produktów po kategoriach

--- Projekcik.NETS/Controllers/ShopController.cs
+++ Projekcik.NETS/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using Projekcik.NETS.Models.Data;
+﻿using Projekcik.NETS.Areas.Admin.Views.Shop;
+using Projekcik.NETS.Models;
+using Projekcik.NETS.Models.Data;
 using Projekcik.NETS.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
@@ -34,6 +36,42 @@ namespace Projekcik.NETS.Controllers
 
             // zwracamy partial z lista
             return PartialView(categoryVMList);
+        }
+
+        public ActionResult Category(string name)
+        {
+            // deklaracja productVMList
+            List<ProductVM> productVMList;
+
+            using (Db db = new Db())
+            {
+                // pobranie id kategorii
+                CategoryDTO categoryDTO = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+                int catId = categoryDTO.Id;
+
+                // inicjalizacja listy produktów
+                productVMList = db.Products
+                                  .ToArray()
+                                  .Where(x => x.CategoryId == catId)
+                                  .Select(x => new ProductVM(x)).ToList();
+
+                // pobieramy nazwe kategori
+               
+                if ((db.Products.Where(x => x.CategoryId == catId).FirstOrDefault()==null))
+                {
+                    ViewBag.CategoryName = categoryDTO.Name;
+                }
+                else
+                {
+                    var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+
+
+                    ViewBag.CategoryName = categoryDTO.Name;
+                }
+            }
+
+            // zwracamy widok z lista produktów z danej kategorii
+            return View(productVMList);
         }
     }
 }
