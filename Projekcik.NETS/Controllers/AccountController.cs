a52f81ac9d02f591cfa2247f48d502b5b3f642a4
Pawel <paawel97@gmail.com>
2020-04-16 20:06:37 +0200

    formatka do logowania, conmtroller, model

--- Projekcik.NETS/Controllers/AccountController.cs
+++ Projekcik.NETS/Controllers/AccountController.cs
@@ -11,8 +11,25 @@ namespace Projekcik.NETS.Controllers
         // GET: Account
         public ActionResult Index()
         {
+            return Redirect("~/account/login");
+        }
+
+        // GET: Account/Login
+        
+        public ActionResult Login()
+        {
+
+            //sprawdzanie czy użytkownik jest zalogowany
+            string userName = User.Identity.Name;
+            if (!string.IsNullOrEmpty(userName))
+            {
+                return RedirectToAction("user-profile");
+            }
+
+            //zwracamy widok logowania
             return View();
         }
+
 
         // GET: Account/create-account
         [ActionName("create-account")]
---
+++ Projekcik.NETS/Models/ViewModels/Account/LoginUserVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Projekcik.NETS.Models.ViewModels.Account
+{
+    public class LoginUserVM
+    {
+        [Required]
+        [Display(Name="Nazwa Użytkownika")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
