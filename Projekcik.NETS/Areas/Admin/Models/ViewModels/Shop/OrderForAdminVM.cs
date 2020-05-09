70423ece873cafaef67ab6bc9177411f4260e8e3
Pawel <paawel97@gmail.com>
2020-05-09 21:40:25 +0200

    ViewModels dla zamówien

---
+++ Projekcik.NETS/Areas/Admin/Models/ViewModels/Shop/OrderForAdminVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projekcik.NETS.Areas.Admin.Models.ViewModels.Shop
+{
+    public class OrderForAdminVM
+    {
+        public int OrderNumber { get; set; }
+        public string UserName { get; set; }
+        public decimal Total { get; set; }
+        public Dictionary<string,int> ProductsAndQnty { get; set; }
+        public DateTime CreeatedAt { get; set; }
+    }
+}
---
+++ Projekcik.NETS/Models/ViewModels/Shop/OrderVM.cs
@@ -0,0 +1,24 @@
+﻿using Projekcik.NETS.Models.Data;
+using System;
+
+namespace Projekcik.NETS.Models.ViewModels.Shop
+{
+
+    public class OrderVM
+    {
+        public OrderVM()
+        {
+
+        }
+        public OrderVM(OrderDTO dto)
+        {
+            OrderId = dto.Id;
+            UserId = dto.UserId;
+            CreateAdT = dto.CreatedAt;
+
+        }
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreateAdT { get; set; }
+    }
+}
